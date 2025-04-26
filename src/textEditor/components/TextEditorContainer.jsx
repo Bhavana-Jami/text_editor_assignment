a7711fe0422d767dbea173c7aa461738ee2fa692
Bhavana-Jam <bhavanajami111@gmail.com>
2025-04-26 21:03:04 +0530

    added toolbar component

--- src/textEditor/components/TextEditorContainer.jsx
+++ src/textEditor/components/TextEditorContainer.jsx
@@ -1,7 +1,11 @@
+import { Toolbar } from "./Toolbar"
 export const TextEditorContainer = () => {
     return (
-        <h1 class="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <div className="container w-4xl h-70 border-2">
+            <Toolbar/>
+            <h1 class="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
     )
 }
